a67581ae039b1ae593e1625e27d304492d4645f3
Alejandro Patino C <apatino@seedem.co>
2021-10-27 22:40:05 -0500

    Full game

--- js/GameConcepts.js
+++ js/GameConcepts.js
@@ -1,5 +1,6 @@
 let startRight = document.querySelector(".startRight");
 let dataConcept;
+let htmlStar = new Array();
 
 const concepts = [{
     draggable: [
@@ -75,10 +76,15 @@ let sizeConcepts = concepts[0].draggable.length;
 let imgDrag, imgDrop;
 let correctAnswers = 0;
 let incorrectAnswers = 0;
+let stars = new Array(sizeConcepts);
+var indices = [];
+let i = 0;
+let nextAnswer = 0
+let ranConcepts = [];
 
 function templateStartLeft(draggable) {
   return `
-    <div id="drag${draggable.id}" data-id="${draggable.dataid}" class="left-hands-0${draggable.class} hand-l">
+    <div id="drag${draggable.id}" data-id="${draggable.dataid}" class="hand-l">
       <div data-id="${draggable.dataid}" class="draggable">
         <img src="${draggable.image}">
       </div>
@@ -88,7 +94,7 @@ function templateStartLeft(draggable) {
 
 function templateStartRight(droppable) {
   return `
-    <div id="drop${droppable.id}" data-id="${droppable.dataid}" class="right-hands-0${droppable.class} hand-r">
+    <div id="drop${droppable.id}" data-id="${droppable.dataid}" class="hand-r">
       <div data-id="${droppable.dataid}" class="droppable">
         <img src="${droppable.image}">
       </div>
@@ -102,6 +108,19 @@ Render();
 
 
 function Render() {
+  
+  // let countAnswers = concepts[0].draggable.length;
+
+  // while(ranConcepts.length < countAnswers) {
+  //   let randIndex = parseInt(Math.floor(Math.random() * countAnswers));
+  //   ranConcepts.push(concepts[randIndex].draggable[0]);
+  //   console.log("ranConcepts "+ JSON.stringify(ranConcepts));
+  // }
+  // concepts[0].draggable.forEach(function(on) {
+
+  //   ranConcepts.push(on);
+  //   console.log(ranConcepts);
+  // })
   for(let i = 0; i < concepts[0].draggable.length; i++) {
     let num = 0;
     imgDrag = concepts[num].draggable[i];
@@ -110,7 +129,29 @@ function Render() {
     // imgTemplate = concepts[0].draggable[i].image[i];
     startRight.innerHTML += `${[imgDrag].map(templateStartLeft).join("")}`;
     // templateStartRight()
+    // let ranConcepts = parseInt(Math.floor(Math.random() * [imgDrag]));
+    // console.log(imgDrag);
+    // console.log(ranConcepts)
+    
+    
   }
+
+ 
+  var myArray = ['1','2','3','4','5'];
+
+  var i,j,k;
+  for (i = myArray.length; i; i--) {
+    j = Math.floor(Math.random() * i);
+    k = myArray[i - 1];
+    myArray[i - 1] = myArray[j];
+    myArray[j] = k;
+  }
+  // console.log(myArray);
+    $('.hand-l').each(function(index) {
+       $(this).addClass( "left-hands-0" + myArray[index]);
+    });
+
+  
 
   for(let i = 0; i < concepts[1].droppable.length; i++) {
     let num = 1;
@@ -121,6 +162,33 @@ function Render() {
     startRight.innerHTML += `${[imgDrop].map(templateStartRight).join("")}`;
     // templateStartRight()
   }
+
+  var rightColumn = ['1','2','3','4','5'];
+
+    var a,b,c;
+    for (a = rightColumn.length; a; a--) {
+      b = Math.floor(Math.random() * a);
+      c = rightColumn[a - 1];
+      rightColumn[a - 1] = rightColumn[b];
+      rightColumn[b] = c;
+    }
+    $('.hand-r').each(function(index2) {
+      console.log("hola")
+      $(this).addClass( "right-hands-0" + rightColumn[index2]);
+   });
+
+  RenderStars();
+  $(".game-wrapper-conceptos").toggleClass("noStars");
+
+  // let countAnswers = concepts[0].draggable.length;
+
+  // while(ranConcepts.length < countAnswers) {
+  //   let randIndex = parseInt(Math.floor(Math.random() * countAnswers));
+  //   ranConcepts.push(concepts[randIndex].draggable[0]);
+  //   console.log("ranConcepts "+ JSON.stringify(ranConcepts));
+  // }
+
+  console.log(imgDrag);
 }
 
 // $('#stage003').animate({
@@ -153,11 +221,39 @@ $(document).ready(function () {
                 bitCorrect = false;
                 listConnections.push({ idConcept: idConcept, idAnswer: idRespuesta });
                 isDragDropConcept = false;
+                console.log(listConnections)
                 // if (sizeConcepts == listConnections.length) {
                 //     validateAnswerConcept(listConnections);
                 // }
+                // if (listConnections.length != 5) {
+                //   ValidateOneAnswer(listConnections);
+                //   // ValidateOneAnswer(1)
+                // }
+
                 if (sizeConcepts == listConnections.length) {
-                  validateAnswerConcept(listConnections)
+                  if(listConnections[nextAnswer].idAnswer === listConnections[nextAnswer].idConcept) {
+                    correctAnswers = correctAnswers + 1;
+                    console.log("es correcto")
+                    fillStar();
+                  }
+                  // $('.footer-orange').attr('style','left: 100%; opacity: 0');
+                  $(".game-wrapper-conceptos").toggleClass("noStars");
+                  
+                  validateAnswerConcept(correctAnswers);
+                  
+                  
+                  $('#stage003, .connector').animate({
+                    left: "-=100%",
+                    opacity: "1"
+                  }, 800);
+                  $('#stage004, .noStars .footer-orange').animate({
+                    left: "-=100%",
+                    opacity: "1"
+                  }, 800);
+                }
+
+                else {
+                  ValidateOneAnswer(listConnections);
                 }
             }
         }
@@ -178,15 +274,117 @@ $(document).ready(function () {
     }, 800)
 })
 
-function validateAnswerConcept(listRelationConcepts) {
-  
-  for(i = 0; i < listRelationConcepts.length; i++) {
-    if(listRelationConcepts[i].idAnswer === listRelationConcepts[i].idConcept) {
-      correctAnswers = correctAnswers + 1;
-    }
-    else {
-      incorrectAnswers = incorrectAnswers + 1;
-    }
+function ValidateOneAnswer(listRelationConcepts) {
+  if(listRelationConcepts[nextAnswer].idAnswer === listRelationConcepts[nextAnswer].idConcept) {
+    correctAnswers = correctAnswers + 1;
+    console.log("es correcto")
+    fillStar();
   }
-  console.log(incorrectAnswers)
+  else {
+    console.log("es incorrecto")
+  }
+  // else {
+  //   incorrectAnswers = incorrectAnswers + 1;
+  // }
+  nextAnswer++;
+  // fillStar();
+  
+}
+
+function validateAnswerConcept(correctAnswers) {
+    // if(listRelationConcepts[i].idAnswer == listRelationConcepts[i].idConcept) {
+    //   correctAnswers = correctAnswers + 1;
+    //   // fillStar();
+    // }
+    // else {
+    //   incorrectAnswers = incorrectAnswers + 1;
+    // }
+    switch(correctAnswers) {
+      case 0:
+        $(".content-info").append("<h6>!No le diste ni a uno solo!</h6>");
+        $(".content-info").append("<p>!No le diste ni a uno solo!</p>");
+        
+        break;
+      case 1:
+        $(".content-info").append("<h6>Te corchaste</h6>");
+        $(".content-info").append("<p>Solo acertaste "+correctAnswers+" de 5 conceptos</p>");
+        
+        // fillStar();
+        break;
+      case 2:
+        $(".content-info").append("<h6>Te corchaste</h6>");
+        $(".content-info").append("<p>Solo acertaste "+correctAnswers+" de 5 conceptos</p>");
+        
+        // fillStar();
+        break;
+      case 3:
+        $(".content-info").append("<h6>Pasaste Raspando</h6>");
+        $(".content-info").append("<p>Solo acertaste "+correctAnswers+" de 5 conceptos</p>");
+        
+        // fillStar();
+        break;
+      case 4:
+        $(".content-info").append("<h6>Pasaste Raspando</h6>");
+        $(".content-info").append("<p>Solo acertaste "+correctAnswers+" de 5 conceptos</p>");
+        
+        // fillStar();
+        break;
+      case 5:
+        $(".content-info").append("<h6>¡Felicitaciones!</h6>");
+        $(".content-info").append("<p>Has relacionado "+correctAnswers+" de 5</p>");
+        
+        // fillStar();
+        break;
+      default:
+        break;
+    }
+
+    $(".content-info").append("<a href='index.html'>Volver a jugar</a>");
+}
+
+function RenderStars() {
+  for(let i = 0; i < stars.length; i++) {
+    htmlStar.push("<img src='images/star-01.svg'>")
+    document.querySelector(".stars-conceptos").innerHTML = htmlStar.join('');
+  }
+}
+
+function fillStar() {
+  console.log(htmlStar)
+  var element = "<img src='images/star-01.svg'>";
+
+  var idx = htmlStar.indexOf(element);
+  while (idx != -1) {
+    indices.push(idx);
+    idx = htmlStar.indexOf(element, idx + 1);
+  }
+  console.log(htmlStar)
+
+  let posicion = indices[0 + i];
+  
+  if(htmlStar[posicion] === "<img src='images/star-01.svg'>") {
+  	htmlStar[posicion] = "<img src='images/star-02.svg'>";
+    console.log(htmlStar);
+    i++;
+    document.querySelector(".stars-conceptos").innerHTML = htmlStar.join('');
+    
+  }
+  
+  /* switch(htmlStar[posicion]) {
+    case '0':
+      htmlStar[posicion] = '1';
+      
+      console.log(htmlStar);
+      i++;
+      starsFooter.innerHTML = htmlStar.join('');
+      htmlStar[posicion + 1];
+      break;
+     case '1':
+       i++;
+      console.log(htmlStar);
+      break;
+  }
+   */
+  console.log(i);
+  console.log(htmlStar.length);
 }
